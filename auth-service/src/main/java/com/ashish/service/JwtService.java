ced93537d4035a810c1011eaa3e4faaa222f0ca9
ASHISHH1823 <ashishmohakud34@gmail.com>
2026-02-26 23:42:15 +0530

    Jwt-Validation-token

--- auth-service/src/main/java/com/ashish/service/JwtService.java
+++ auth-service/src/main/java/com/ashish/service/JwtService.java
@@ -1,9 +1,13 @@
 package com.ashish.service;
 
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.ashish.entity.Appuser;
 
 public interface JwtService {
 	public String generateToken(Appuser appuser);
+	public String extractUsername(String token);
+	public boolean validateToken(String token,UserDetails userDetails);
 
 }
