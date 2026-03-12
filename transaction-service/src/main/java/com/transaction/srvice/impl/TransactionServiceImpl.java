19c0bf6db568378816209b9356a92ef34c449a65
ASHISHH1823 <ashishmohakud34@gmail.com>
2026-03-12 22:26:09 +0530

    Transaction_service-Businesslogic-Implemanted

--- transaction-service/src/main/java/com/transaction/srvice/impl/TransactionServiceImpl.java
+++ transaction-service/src/main/java/com/transaction/srvice/impl/TransactionServiceImpl.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import com.transaction.clien.AccountClient;
+import com.transaction.dto.AccountResponse;
+import com.transaction.dto.BalanceUpdateRequest;
 import com.transaction.dto.TransferRequest;
+import com.transaction.exception.holder.InsufficientBalanceException;
 import com.transaction.repo.TransactionRepo;
 import com.transaction.srvice.TransactionService;
 
@@ -19,7 +22,20 @@ public class TransactionServiceImpl implements TransactionService{
 
 	@Override
 	public String transfer(TransferRequest transferRequest) {
+		  AccountResponse sender = 
+				  accountClient.getDetails(transferRequest.getFromAccount());
 		  
+		  AccountResponse receiver = 
+				  accountClient.getDetails(transferRequest.getToAccount());
+		  
+		  if(sender.getBalance()< transferRequest.getAmount()) {
+			  throw new InsufficientBalanceException("Insufficient balance");
+		  }
+		  BalanceUpdateRequest Debit = 
+				  new BalanceUpdateRequest(transferRequest.getFromAccount(),-transferRequest.getAmount());
+		BalanceUpdateRequest Credit = 
+				new BalanceUpdateRequest(transferRequest.getToAccount(),-transferRequest.getAmount());
+		
 		
 		
 		return null;
