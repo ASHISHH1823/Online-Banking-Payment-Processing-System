852896e3aa88988f989fdd87595eaecde9175ef0
ASHISHH1823 <ashishmohakud34@gmail.com>
2026-03-20 01:00:59 +0530

    api-gateway

--- api-gateway/src/main/java/com/ashish/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ashish/ApiGatewayApplication.java
@@ -1,8 +1,12 @@
 package com.ashish;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
+	public static void main(String args[]) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
 
 }
---
+++ api-gateway/src/main/java/com/ashish/service/JwtService.java
@@ -0,0 +1,6 @@
+package com.ashish.service;
+
+public class JwtService {
+	private final String SECRET="";
+
+}
