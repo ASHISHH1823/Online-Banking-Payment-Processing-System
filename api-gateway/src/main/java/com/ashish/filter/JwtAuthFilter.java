eb279c4cda50384f6b741125c956ec2e2daeb7a9
ASHISHH1823 <ashishmohakud34@gmail.com>
2026-03-25 23:09:42 +0530

    implement-security-account-service

--- api-gateway/src/main/java/com/ashish/filter/JwtAuthFilter.java
+++ api-gateway/src/main/java/com/ashish/filter/JwtAuthFilter.java
@@ -1,6 +1,8 @@
 package com.ashish.filter;
 
 
+import java.util.List;
+
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
@@ -14,13 +16,20 @@ import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 
 @Component
-@RequiredArgsConstructor
-public class JwtAuthFilter extends AbstractGatewayFilterFactory<Object> {
+
+public class JwtAuthFilter extends AbstractGatewayFilterFactory<JwtAuthFilter.Config> {
 
     private final JwtService jwtService;
+    
+    public static class Config{}
+    
+    public JwtAuthFilter(JwtService jwtService) {
+		super(Config.class);
+		this.jwtService=jwtService;
+    }
 
     @Override
-    public GatewayFilter apply(Object config) {
+    public GatewayFilter apply(Config config) {
 
         return (exchange, chain) -> {
 
@@ -42,12 +51,20 @@ public class JwtAuthFilter extends AbstractGatewayFilterFactory<Object> {
 
             Claims claims = jwtService.validateToken(token);
             String role = jwtService.extractRole(claims);
-
-            if (path.contains("/activate") || path.contains("/block")) {
-                if (!"ROLE_ADMIN".equals(role)) {
-                    throw new AccessDeniedException("ADMIN only");
-                }
-            }
+            String username = claims.getSubject();
+            
+            exchange = exchange.mutate()
+                    .request(exchange.getRequest().mutate()
+                            .header("User", username)
+                            .header("Role", role)
+                            .build())
+                    .build();
+            List<String> adminPaths =List.of("/active","/block");
+            if (adminPaths.stream().anyMatch(path::contains)) {
+				if (!"ROLE_ADMIN".equals(role)) {
+					throw new AccessDeniedException("ADMIN only");
+				}
+			}
 
             return chain.filter(exchange);
         };
